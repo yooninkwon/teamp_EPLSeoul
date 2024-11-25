9508e55c71c85b93eeebc14b2d783f68132c7bf1
Monica-Jang <monicajang95@gmail.com>
2024-11-25 16:52:29 +0900

    second commit

---
+++ src/main/java/com/tech/EPL/ProjectEplSeoulApplication.java
@@ -0,0 +1,13 @@
+package com.tech.EPL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEplSeoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEplSeoulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/EPL/ProjectEplSeoulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.EPL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEplSeoulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
