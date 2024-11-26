a88d4edc68f274692fb9f0fb3e8bacad5c6d8a52
dpoowa <gukmunga@gmail.com>
2024-11-26 13:41:07 +0900

    - WebClient로 XML 데이터 수신을 위한 라이브러리 의존성 추가
    - WebClient Bean 생성: JSON과 XML 각각 별도의 메서드로 나누어 처리
    
    - WebClient 연결 관리: LoopResources와 ConnectionProvider를 Bean으로 생성하여 WebClient Config에 주입
    
    - WebClient Config에서 DisposableBean 구현: 프로그램 종료 시 LoopResources와 ConnectionProvider가 안전하게 종료되도록 처리
    
    - 법정동 코드와 구 코드 저장을 위한 Enum 클래스 생성
    
    - "구 코드 서비스"와 "동 코드 서비스" 생성 후 "서비스그룹" 클래스에 주입하여 캡슐화

--- src/main/java/com/tech/EPL/realty/controller/ELPRealtyController.java
+++ src/main/java/com/tech/EPL/realty/controller/ELPRealtyController.java
@@ -22,3 +22,5 @@ public class ELPRealtyController {
 		serviceGroup.testMethod(model);
 	}
 }
+
+
--- src/main/java/com/tech/EPL/realty/service/group/RealtyServiceGroup.java
+++ src/main/java/com/tech/EPL/realty/service/group/RealtyServiceGroup.java
@@ -3,6 +3,10 @@ package com.tech.EPL.realty.service.group;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
+import com.tech.EPL.realty.code.DongCode;
+import com.tech.EPL.realty.code.GuCode;
+import com.tech.EPL.realty.service.DongCodeService;
+import com.tech.EPL.realty.service.GuCodeService;
 import com.tech.EPL.realty.service.TestService;
 
 import lombok.RequiredArgsConstructor;
@@ -11,9 +15,28 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RealtyServiceGroup {
 	private final TestService testService;
+	private final DongCodeService dongCodeService;
+	private final GuCodeService guCodeService;
 	
 	public void testMethod(Model model) {
 		testService.execution(model);
 	}
+	
+	public DongCode searchDongCode(int code) {
+		return dongCodeService.findByDongCode(code);
+	}
+	
+	public DongCode searchDongName(String name) {
+		return dongCodeService.findByDongName(name);
+	}
+	
+	public GuCode searchGuCode(int code) {
+		return guCodeService.findByGuCode(code);
+	}
+	
+	public GuCode searchGuName(String name) {
+		return guCodeService.findByGuName(name);
+	}
+	
 	
 }
