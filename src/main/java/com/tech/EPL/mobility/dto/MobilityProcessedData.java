39aa6a4c20dcdcaeb26c98e3125c257eb164eaf0
Monica-Jang <monicajang95@gmail.com>
2024-12-09 12:47:54 +0900

    Spring batch Config활용 DB 저장
    >의존성 및 propertis 설정 추가
    - 원본 파일 경로 고정 주입
    - batch관련 스키마 자동 생성
    
    >Test 진행
    - Controller : 트리거 작동 및 Service 호출 검증 완료
    - Service : 경로 및 파일 유효성 검사 및 파라미터 전달 검증 완료
    - Configuration : csvReader, Processor, Writer 동작 검증 완료(*jsonReader 선언 타입 수정 필요)
    
    >실제 csv파일 파싱 및 DB저장
    - 최종 가공 데이터 652개 저장 확인 완료

--- src/main/java/com/tech/EPL/mobility/dto/MobilityProcessedData.java
+++ src/main/java/com/tech/EPL/mobility/dto/MobilityProcessedData.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 @Table(name = "MOBILITY_SBD") // 테이블명 매핑
 @Getter
 @Setter
-public class ProcessedData {
+public class MobilityProcessedData {
 	@Id // 기본 키 설정
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 증가
 	private Integer no;
@@ -21,6 +21,7 @@ public class ProcessedData {
     private String name;
     private double lat;
     private double lot;
+    private String si;
     private String gu;
     private String dong;
 }
